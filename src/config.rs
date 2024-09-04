72ec03841fc1f2c7860991e6bd186c5a14b17486
ParkSnoopy <117149837+ParkSnoopy@users.noreply.github.com>
2024-09-04 21:01:43 +0900

    Update config.rs
    url config change

--- src/config.rs
+++ src/config.rs
@@ -4,5 +4,5 @@ pub const PARALLEL: u16 = 16;
 pub const DL_LEVEL: u16 = 1;
 
 pub const DEFAULT_CONFIG_JSON_LOCAL: &str = "./config/default.json"; // start from cmd pwd
-pub const DEFAULT_CONFIG_JSON_REMOTE_BASE_URL: &str = "https://raw.githubusercontent.com/mil-acc-github/sgb_initializer/main/config/";
+pub const DEFAULT_CONFIG_JSON_REMOTE_BASE_URL: &str = "https://raw.githubusercontent.com/ParkSnoopy/sgb_initializer/main/config/";
 pub const DEFAULT_CONFIG_JSON_REMOTE_FILENAME: &str = "default.json";
