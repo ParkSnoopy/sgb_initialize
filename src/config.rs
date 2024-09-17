2da11e49130ff3aaf5d2a66b9051dd9c23c63b05
ParkSnoopy <117149837+ParkSnoopy@users.noreply.github.com>
2024-09-17 19:31:34 +0900

    update: post-download idle second change 5->20

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
-pub const POST_IDLE: u64 = 5;
+pub const POST_IDLE: u64 = 20;
 
 pub const CONFIG_JSON_LOCAL_PATH: &str = "./config/default.json"; // start from cmd pwd
 
 pub const CONCURRENT_DOWNLOAD: u16 = 16;
-pub const DEFAULT_CONFIG_JSON_REMOTE_BASE_URL: &str = "https://raw.githubusercontent.com/mil-acc-github/sgb_initializer/main/config/";
+pub const DEFAULT_CONFIG_JSON_REMOTE_BASE_URL: &str = "https://raw.githubusercontent.com/ParkSnoopy/sgb_initializer/main/config/";
 pub const DEFAULT_CONFIG_JSON_REMOTE_FILENAME: &str = "default.json";
