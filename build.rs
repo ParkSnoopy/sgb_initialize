fe51091ed67f8d4bcf964c19ecd89fe040eac503
ParkSnoopy <117149837+ParkSnoopy@users.noreply.github.com>
2024-09-17 19:14:52 +0900

    update: this program doesn't require windows' UAC

--- build.rs
+++ build.rs
@@ -8,19 +8,7 @@ fn main() -> Result<()> {
 
         let mut res = winres::WindowsResource::new();
 
-        res.set_manifest(r#"
-<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-    <security>
-      <requestedPrivileges>
-        <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
-      </requestedPrivileges>
-    </security>
-  </trustInfo>
-</assembly>
-        "#);
-
-        res.set_icon("assets/icon.ico");
+        // res.set_icon("assets/icon.ico");
 
         res.compile()?;
     }
